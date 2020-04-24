03708fa1259b486a8b151fc69ac495ec189d8c03
Nicole Zheng <zznicole@hotmail.com>
2020-04-24 22:37:39 +0200

    solve class, extends, super tasks

--- src/script.js
+++ src/script.js
@@ -9,35 +9,50 @@ Also add a method that refills the marker. Demonstrate the work of these methods
 */
 
 class RegularMarker {
-  constructor(color, inkAmount, text) {
+  constructor(color, inkAmount) {
     this.color = color;
     this.inkAmount = inkAmount;
-    this.text = text;
-    }
+  }
 
-  displayColor () { 
+  print(text) { 
     if (this.inkAmount > 0) {
-    document.write(`<p style= "color:${this.color}">${this.text}</p>`);
+      document.write(`<p style= "color:${this.color}">${text}</p>`);
     } else {
       document.write(`<p></p>`);
     }
+  
+   let textcharactors = text.replace(/ /g,'').length;
+   this.inkAmount = this.inkAmount - textcharactors * 0.5;
+  
   }
 }
 
-let marker1 = new RegularMarker ('pink', 20, 'The Marker has ink!');
-marker1.displayColor();
+let marker = new RegularMarker ('Blue', 100);
+
+marker.print('Hello Spring!');
+marker.print('Spring is beautiful! There are so many things that deserve your attention.');
+marker.print('At this time of year later. This time of year, we also can grow plants on our balcony.');
+marker.print('1.some your favourite herbs and flowers will bring  nice scent!');
+marker.print('2.some your favourite herbs and flowers will bring  nice scent!');
+marker.print('3.some your favourite herbs and flowers will bring  nice scent!');
 
 class RefillableMarker extends RegularMarker {
-  constructor(inkAmount) {
-    super('pink', inkAmount);
+  constructor(color, inkAmount) {
+    super(color, inkAmount);
   }
 
-get refill () {
-    if (this.inkAmount = 0 ) {
-    document.write(`<p style= "color:${this,color}">${this.text}</p>`); 
-    }
+  refill(text) {
+    this.inkAmount = 100;
+    document.write(`<p style= "color:${this.color}">${text}</p>`); 
   }
 }
 
-let refilledMarker = new RegularMarker (30);
-refilledMarker.refill;
+let refilledMarker = new RefillableMarker('blue', 100);
+
+refilledMarker.refill('Hello Again!');
+refilledMarker.refill('Hello Again!Spring is beautiful! There are so many things that deserve your attention.');
+refilledMarker.refill('Hello Again!At this time of year later. This time of year, we also can grow plants on our balcony.');
+refilledMarker.refill('Hello Again!At this time of year later. This time of year, we also can grow plants on our balcony.');
+refilledMarker.refill('Hello Again!At this time of year later. This time of year, we also can grow plants on our balcony.');
+refilledMarker.refill('Hello Again!At this time of year later. This time of year, we also can grow plants on our balcony.');
+
